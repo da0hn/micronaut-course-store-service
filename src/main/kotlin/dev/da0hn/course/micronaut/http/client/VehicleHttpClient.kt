e06b84410f2e6a7a654b1685a3aec2b0bfa3b1a8
Gabriel Honda <gabrielhondacba@gmail.com>
2024-02-04 10:33:28 -0400

    fix: utiliza `@CircuitBreaker` ao invés de `@Retryable`

--- src/main/kotlin/dev/da0hn/course/micronaut/http/client/VehicleHttpClient.kt
+++ src/main/kotlin/dev/da0hn/course/micronaut/http/client/VehicleHttpClient.kt
@@ -3,13 +3,12 @@ package dev.da0hn.course.micronaut.http.client
 import dev.da0hn.course.micronaut.http.client.dto.VehicleDto
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.retry.annotation.CircuitBreaker
 import io.micronaut.retry.annotation.Recoverable
-import io.micronaut.retry.annotation.Retryable
-
 
 
 @Recoverable // Since Micronaut Framework 4.0, declarative clients annotated with @Client no longer invoke fallbacks by default. To restore the previous behaviour add the following dependency and annotate any declarative clients with @Recoverable.
-@Retryable(attempts = "3")
+@CircuitBreaker(attempts = "3")
 @Client(id = "http://localhost:8080", path = "/vehicles")
 interface VehicleHttpClient {
 
